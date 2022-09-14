f20da966db55a75fc582f988241e2e6a9dadf923
red <106205704+ilyred@users.noreply.github.com>
2022-09-14 18:00:42 +0100

    Update utils.lua

--- utils.lua
+++ utils.lua
@@ -1,3 +1,44 @@
 # made by red
 
 
+local function getpath(item)
+	v = item
+	p = item.Name .. "."
+	while (true) do
+		if not v.Parent.Parent then
+			p = p .. v.Parent.Name
+			break
+		else
+			p = p .. v.Parent.Name .. "."
+		end
+		v = v.Parent
+	end
+	return p
+end
+
+
+local function cframetov3(cf) 
+   return Vector3.new(cf.X, cf.Y, cf.Z)
+end
+
+
+local function resolvename(name)
+    if not players:FindFirstChild(name) then
+        for _,player in pairs(players:GetPlayers()) do
+    		if player.DisplayName == display then return player.name end
+        end
+	    return ""
+	else
+        return name
+    end
+end
+
+
+local function displaytoname(display) 
+	for _,player in pairs(players:GetPlayers()) do
+		if player.DisplayName == display then
+			return player.name
+		end
+	end
+	return ""
+end
